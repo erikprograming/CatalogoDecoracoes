7ef24cae353535acd3e07e9da6ad0a22d93d6e2b
erikprograming <erikaomende@gmail.com>
2025-07-30 11:41:09 -0300

    Update Arcos.js

--- Interaction/Arcos.js
+++ Interaction/Arcos.js
@@ -47,7 +47,7 @@ fetch('https://api.github.com/repos/erikprograming/CatalogoDecoracoes/contents/I
             document.getElementById('galeria').appendChild(div);
 
             i++;
-            setTimeout(carregarProxima, 100); // delay de 200ms entre cada imagem
+            setTimeout(carregarProxima, 225); // delay de 200ms entre cada imagem
         }
 
         carregarProxima();
